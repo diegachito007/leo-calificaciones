4b76543502fd88a70c87fe2da7f01eae8e990ba8
Diego Yamberla <difabians@hotmail.com>
2026-09-12 13:20:39 -0500

    mejorando logos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -340,7 +340,7 @@ export default function Dashboard() {
                 </div>
               ) : (
                 <img
-                  src="/dayaniX-eduX-i.png"
+                  src="/logo.dayaniX.jpg"
                   alt="dayaniX"
                   className="h-16 w-auto object-contain"
                   onError={() => setLogoError(true)}
